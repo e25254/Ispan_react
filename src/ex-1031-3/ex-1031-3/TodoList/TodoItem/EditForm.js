ea635cc1cad22435a259a9c20ddb405deef4cb54
Jie-Mac <e25254@gmail.com>
2022-11-01 09:46:46 +0800

    模組化

---
+++ src/ex-1031-3/ex-1031-3/TodoList/TodoItem/EditForm.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+function EditForm({ id, updateTodo, text }) {
+  // 使用衍生的props作為state初始值
+  const [inputEditingValue, setInputEditingValue] = useState(text)
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputEditingValue}
+        onChange={(e) => {
+          setInputEditingValue(e.target.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          updateTodo(id, {
+            text: inputEditingValue,
+            editing: false,
+          })
+        }}
+      >
+        儲存
+      </button>
+    </>
+  )
+}
+
+export default EditForm
