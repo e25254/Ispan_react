6a4f6b24aca593657015cba5ca6f7961e3bcd5e1
Jie-Mac <e25254@gmail.com>
2022-11-01 13:02:09 +0800

    密碼檢視用按鈕取代checkbox

--- src/ex-1101-1/MultipleInput.js
+++ src/ex-1101-1/MultipleInput.js
@@ -1,27 +1,57 @@
 import { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
+import { FaEyeSlash } from 'react-icons/fa';
+import './MultipleInput.css';
 
 function MultipleInput() {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+	// const [username, setUsername] = useState('');
+	// const [password, setPassword] = useState('');
+
+	const [user, setUser] = useState({
+		username: '',
+		password: '',
+	});
+
+	// true = 呈現密碼	false = 隱藏密碼
+	const [show, setShow] = useState(false);
+
+	const handleFieldChange = (e) => {
+		console.log(e.target.type, e.target.name, e.target.value);
+		const newUser = { ...user, [e.target.name]: e.target.value };
+		setUser(newUser);
+	};
+
 	return (
 		<>
 			<label>帳號：</label>
-			<input
-				type="text"
-				value={username}
-				onChange={(e) => {
-					setUsername(e.target.value);
-				}}
-			/>
+			<input type="text" name="username" value={user.username} onChange={handleFieldChange} />
 			<br />
-			<label>密碼：</label>
-			<input
-				type="text"
-				value={password}
-				onChange={(e) => {
-					setPassword(e.target.value);
+			<div className="MultipleInput_passwordBox">
+				<label>密碼：</label>
+				<input className="MultipleInput_password" type={show ? 'text' : 'password'} name="password" value={user.password} onChange={handleFieldChange} />
+				<label className="MultipleInput_eye">
+					<FaEye
+						className={show ? '' : 'MultipleInput_hidden'}
+						onClick={() => {
+							setShow(false);
+						}}
+					/>
+					<FaEyeSlash
+						className={!show ? '' : 'MultipleInput_hidden'}
+						onClick={() => {
+							setShow(true);
+						}}
+					/>
+				</label>
+			</div>
+			{/* <input
+				type="checkbox"
+				checked={show}
+				onChange={() => {
+					setShow(!show);
 				}}
-			/>
+			/> */}
+			{/* <label>顯示密碼</label> */}
 		</>
 	);
 }
