1bd4bd0f9e1d7f460d13266f82dbd910faf5de2b
Jie-pc <e25254@gamil.com>
2022-11-03 00:38:10 +0800

    好難

--- src/testComponent/App_DatePicker.js
+++ src/testComponent/App_DatePicker.js
@@ -21,8 +21,8 @@ const App_DatePicker = () => {
 					bordered
 					onChange={(e) => {
 						moment.locale('zh-tw');
-						let start = moment(e[0]._d).format('YYYY/MM/DD');
-						let end = moment(e[1]._d).format('YYYY/MM/DD');
+						let start = moment(e[0]._d).format('YYYY-MM-DD');
+						let end = moment(e[1]._d).format('YYYY-MM-DD');
 						let howLong =
 							(+new Date(end) - +new Date(start)) / 86400000;
 						console.log(howLong);
@@ -37,14 +37,35 @@ const App_DatePicker = () => {
 			<input value={pickDate.startTime} readOnly />
 			<input value={pickDate.endTime} readOnly />
 			<input value={pickDate.days + '晚'} readOnly />
+			<Space direction="vertical" size={12} className={'hiddenBox'}>
+				<RangePicker
+					open={true}
+					className={'noShow'}
+					locale={locale}
+					bordered
+					onChange={(e) => {
+						moment.locale('zh-tw');
+						let start = moment(e[0]._d).format('YYYY-MM-DD');
+						let end = moment(e[1]._d).format('YYYY-MM-DD');
+						let howLong =
+							(+new Date(end) - +new Date(start)) / 86400000;
+						console.log(howLong);
+						setPickDate({
+							startTime: start,
+							endTime: end,
+							days: howLong,
+						});
+					}}
+				/>
+			</Space>
 			{/* TODO: 新增一個大日曆 兩個日曆能夠連動 */}
-			<div className="site-calendar-demo-card">
+			{/* <div className="site-calendar-demo-card">
 				<Calendar
 					fullscreen={false}
 					onPanelChange={onPanelChange}
 					value={moment('2022-03-03')}
 				/>
-			</div>
+			</div> */}
 		</>
 	);
 };
