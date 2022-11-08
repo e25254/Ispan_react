bd19c8ea74870eb30e6a642f8dc6c6ffdfc1c448
Jie-Mac <e25254@gmail.com>
2022-11-08 12:04:15 +0800

    detail&notice

--- src/TimeTravel/TimeTravel_Hotel.js
+++ src/TimeTravel/TimeTravel_Hotel.js
@@ -6,6 +6,8 @@ import Breadcrumb from './Breadcrumb/Breadcrumb';
 import Rate from './Rate/Rate';
 import IconBar from './IconBar/IconBar';
 import ShowPic from './ShowPic/ShowPic';
+import HotelNotice from './HotelNotice/HotelNotice';
+import HotelDetail from './HotelDetail/HotelDetail';
 
 function TimeTravel_Hotel() {
 	return (
@@ -21,8 +23,15 @@ function TimeTravel_Hotel() {
 				<Rate />
 				<IconBar />
 				<h4 style={{ color: '#59d8a1' }}>{/* TODO:拿到真實價格 */}TWD$599</h4>
-				<h4 style={{ color: '#4D4D4D', margin: '20px 0px' }}>房型介紹</h4>
+				<hr />
+				<h2 style={{ color: '#4D4D4D', margin: '30px 0px' }}>房型介紹</h2>
 				<ShowPic />
+				<hr />
+				<h2 style={{ color: '#4D4D4D', margin: '30px 0px' }}>注意事項</h2>
+				<HotelNotice />
+				<hr />
+				<h2 style={{ color: '#4D4D4D', margin: '30px 0px' }}>商品說明</h2>
+				<HotelDetail />
 			</div>
 		</>
 	);
